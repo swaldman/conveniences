65b7ff368b96056b729ab98870a328555043880c
Steve Waldman <swaldman@mchange.com>
2023-12-16 18:42:28 -0500

    Add conversion extensions between Map[String,String] and Properties.

--- src/com/mchange/conveniences/javautil/core.scala
+++ src/com/mchange/conveniences/javautil/core.scala
@@ -4,6 +4,7 @@ import java.io.{BufferedInputStream, File, FileInputStream}
 import java.nio.file.Path
 import java.util.Properties
 import scala.util.Using
+import scala.jdk.CollectionConverters.*
 
 def loadProperties( file : File, defaults : Option[Properties] ) : Properties =
   Using.resource( new BufferedInputStream( new FileInputStream( file ) ) ): is =>
@@ -14,3 +15,12 @@ def loadProperties( file : File, defaults : Option[Properties] ) : Properties =
 def loadProperties( file : File ) : Properties                                = loadProperties(file, None)
 def loadProperties( path : Path, defaults : Option[Properties] ) : Properties = loadProperties( path.toFile(), defaults )
 def loadProperties( path : Path ) : Properties                                = loadProperties( path, None )
+
+extension ( p : Properties )
+  def toMap : Map[String,String] = p.asScala.toMap
+
+extension ( m : Map[String,String] )
+  def toProperties : Properties =
+    val p = new Properties()
+    m.foreach( (k,v) => p.setProperty(k,v) )
+    p
