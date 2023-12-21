14d6ebe6bb58da42bd018e2ab1db34803a284802
Steve Waldman <swaldman@mchange.com>
2023-12-21 14:42:18 -0500

    Define uniqueOr(...) extension method for Set/Seq.

---
+++ src/com/mchange/conveniences/collection/core.scala
@@ -0,0 +1,15 @@
+package com.mchange.conveniences.collection
+
+import scala.collection.*
+
+object NotUnique:
+  case object Empty extends NotUnique
+  case class Multiple( n : Int ) extends NotUnique
+sealed trait NotUnique
+
+extension[T,U <: Seq[T] | Set[T]] ( c : U )
+  def uniqueOr( notUnique : (U, NotUnique) => T ) : T =
+    c.size match
+      case 0 => notUnique( c, NotUnique.Empty )
+      case 1 => c.head
+      case n => notUnique( c, NotUnique.Multiple(n) )
