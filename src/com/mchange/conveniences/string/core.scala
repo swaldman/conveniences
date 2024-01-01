119d711278d55038a3639539d94fa37ebb2968a5
Steve Waldman <swaldman@mchange.com>
2024-01-01 12:41:02 -0500

    Add pathJoin(...) string convenience.

--- src/com/mchange/conveniences/string/core.scala
+++ src/com/mchange/conveniences/string/core.scala
@@ -13,4 +13,12 @@ extension ( s : String )
 
   def toOptionNotBlank : Option[String] = toOptionNotBlank( true )
 
+  def pathJoin( pathSeparator : Char )( path0 : String, path1 : String ) : String =
+    val firstEndsWith    = path0.last == pathSeparator
+    val secondBeginsWith = path1.head == pathSeparator
+    (firstEndsWith, secondBeginsWith ) match
+      case (true, true)                  => path0 + path1.tail
+      case (true, false) | (false, true) => path0 + path1
+      case (false, false)                => (path0 + '/') + path1
 
+  def pathJoin( path0 : String, path1 : String ) : String = pathJoin('/')(path0, path1)
