e3c5b844d6a256ff42ef07b8daa3781256d78e27
Steve Waldman <swaldman@mchange.com>
2024-06-01 22:18:16 -0400

    Define tf extension to boolean.

--- src/com/mchange/conveniences/boolean/core.scala
+++ src/com/mchange/conveniences/boolean/core.scala
@@ -4,3 +4,5 @@ val UnitOption : Option[Unit] = Some( () )
 
 extension ( b : Boolean )
   def toOption : Option[Unit] = if b then UnitOption else None
+  def tf[A]( ifTrue : =>A)( ifFalse : =>A ) = if b then ifTrue else ifFalse
+
