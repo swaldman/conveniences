cca231ce2247043bc6d79124e2ed1cf61b6ac257
Steve Waldman <swaldman@mchange.com>
2025-09-29 20:49:53 -0400

    Unicode-whitespace-capable trim / blank functions, more resilient parseCommaListXXX.

--- src/com/mchange/conveniences/string/core.scala
+++ src/com/mchange/conveniences/string/core.scala
@@ -1,21 +1,38 @@
 package com.mchange.conveniences.string
 
+// Requires features not always supported by scalajs
+// private val TrimHardRegex = """(?:^\p{Z}+)|(?:\p{Z}+$)""".r
+
+private val TrimHardRegex =
+  val spaceCharClass = """[\s\u00A0\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u200b\u202F\u205F\u3000\uFEFF]"""
+  raw"""(?:^${spaceCharClass}+)|(?:${spaceCharClass}+$$)""".r
+
 extension ( s : String )
+  def trimUnicode : String = TrimHardRegex.replaceAllIn(s,"")
+
   def nullOrEmpty : Boolean = s == null || s.isEmpty
 
   def nullOrBlank : Boolean = s == null || s.trim.isEmpty
 
-  def toOptionNotBlank( trim : Boolean ) : Option[String] =
+  def nullOrBlankUnicode : Boolean = s == null || s.trimUnicode.isEmpty
+
+  private inline def toOptionNotBlank( inline unicode : Boolean)( trim : Boolean ) : Option[String] =
     if s == null then
       None
     else
-      val trimmed = s.trim()
+      val trimmed = inline if unicode then s.trimUnicode else s.trim
       if trimmed.nonEmpty then
         Some( if trim then trimmed else s )
       else
         None
 
+  def toOptionNotBlank( trim : Boolean ) : Option[String] = toOptionNotBlank( false )( trim )
+
   def toOptionNotBlank : Option[String] = toOptionNotBlank( true )
+
+  def toOptionNotBlankUnicode( trim : Boolean ) : Option[String] = toOptionNotBlank( true )( trim )
+
+  def toOptionNotBlankUnicode : Option[String] = toOptionNotBlankUnicode( true )
 
 def pathJoin( pathSeparator : Char )( path0 : String, path1 : String ) : String =
   val firstEndsWith    = path0.last == pathSeparator
@@ -36,5 +53,17 @@ private def commaListXXX(xxx : String)( seq : Seq[String] ) : Option[String] =
       val anded = seq.init :+ s"$xxx ${seq.last}"
       Some( anded.mkString(", ") )
 
+private def parseCommaListXXX(xxx : String)( listText : String ) : Seq[String] =
+  val uncommaed : Array[String] = listText.split("""\s*\,\s*""").flatMap( _.split(raw"""\s*\b${xxx}\b\s*""") ).filter( _.trimUnicode.nonEmpty )
+  println( uncommaed.mkString("|") )
+  if uncommaed.length == 1 then
+    Seq(uncommaed(0))
+  else
+    (uncommaed.init :+ uncommaed.last.stripPrefix(xxx).trim).toSeq
+
 def commaListAnd( seq : Seq[String] ) : Option[String] = commaListXXX("and")(seq)
 def commaListOr( seq : Seq[String] )  : Option[String] = commaListXXX("or")(seq)
+
+def parseCommaListAnd( listText : String ) = parseCommaListXXX("and")(listText)
+def parseCommaListOr( listText : String ) = parseCommaListXXX("or")(listText)
+
